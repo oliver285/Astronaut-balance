016b12e455741dda62a632f046f8c75a9a8af6b3
Crawfoed <etcr5553@colorado.edu>
2024-12-09 10:08:59 -0700

    Create Parameters_3_Tether_Dynamic_Model.py

---
+++ modeling/dynamic_models/Parameters_3_Tether_Dynamic_Model.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class Parameters:
+    mass = 200.0  # person's weight (lb)
+    r = 3 / (2 * np.pi)  # waist radius (ft)
+    teth_percent_error = 0.05  # percent error in tether length
+
+    # tether anchor loc 3x3 each row is the vector for each tether
+    # assuming anchor locations are radially 2 feet away from person 120 degrees away from each other
+    '''
+    teth_anchor = [[2,                      0.0,                     0.0],
+                  [-2.0, -2.0, 0.0],
+                  [-2.0, 2.0, 0.0]]
+    # attachment offset vector 3x3 each row is the vector for each tether
+    # assuming circular radius (pointing from tether attachment loc to COM
+    offset = [[-r,                      0.0,                     0.0],
+              [-r*np.cos(225*np.pi/180), -r*np.sin(225*np.pi/180), 0.0],
+              [-r*np.cos(135*np.pi/180), -r*np.sin(135*np.pi/180), 0.0]]
+    '''
+    teth_anchor = [[-2,                      0.0,                     0.0],
+                  [2.0, -2.0, 0.0],
+                  [2.0, 2.0, 0.0]]
+    offset = [[r,                      0.0,                     0.0],
+              [-r*np.cos(315*np.pi/180), -r*np.sin(315*np.pi/180), 0.0],
+              [-r*np.cos(45*np.pi/180), -r*np.sin(45*np.pi/180), 0.0]]
+              
+    dt = 0.002
+    force_update_rate = 0.004
+    reaction_t = 0.01
