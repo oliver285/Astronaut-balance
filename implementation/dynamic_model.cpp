eb1e9a00a3c95664482e1cde9891c00b19c5436f
Your Name <your.email@example.com>
2025-02-02 16:23:48 -0700

    updated using eigen

--- implementation/dynamic_model.cpp
+++ implementation/dynamic_model.cpp
@@ -46,9 +46,10 @@
  */
 
 #include "ClearCore.h"
-#include"/BasicLinearAlgebra/BasicLinearAlgebra.h"
 #include <cmath>
-//#include <Eigen/Dense>
+#include<ArduinoEigenDense.h>
+
+using namespace Eigen;
 // The INPUT_A_FILTER must match the Input A filter setting in
 // MSP (Advanced >> Input A, B Filtering...)
 #define INPUT_A_FILTER 20
@@ -59,190 +60,193 @@
 // Select the baud rate to match the target device.
 #define baudRate 9600
 
-using namespace std;
+//using namespace std;
 
 // Defines the limit of the torque command, as a percent of the motor's peak
 // torque rating (must match the value used in MSP).
 double maxTorque = 100;
 
-/*def equations(p, a, b, c, r):
-    x, y, z = p
-    return (
-        x**2 + (y-(2-r))**2 + z**2 - a**2,
-        (x-(2-r)*np.cos(210*np.pi/180))**2 + (y-(2-r)*np.sin(210*np.pi/180))**2 + z**2 - b**2,
-        (x-(2-r)*np.cos(330*np.pi/180))**2 + (y-(2-r)*np.sin(330*np.pi/180))**2 + z**2 - c**2
-    )
+/*def calculate_tether_vecs(COM, teth_anchor, offset):
+    # determine the tether unit vector
+    teth1_vec = np.array(teth_anchor[0][:]) - (np.array(COM) - np.array(offset[0][:]))
+    teth2_vec = np.array(teth_anchor[1][:]) - (np.array(COM) - np.array(offset[1][:]))
+    teth3_vec = np.array(teth_anchor[2][:]) - (np.array(COM) - np.array(offset[2][:]))
+    teth1_hat = teth1_vec/np.linalg.norm(teth1_vec)
+    teth2_hat = teth2_vec/np.linalg.norm(teth2_vec)
+    teth3_hat = teth3_vec/np.linalg.norm(teth3_vec)
+    lengths = np.array([np.linalg.norm(teth1_vec),np.linalg.norm(teth2_vec),np.linalg.norm(teth3_vec)])
+
+    return (teth1_hat, teth2_hat, teth3_hat, lengths)
+
 */
 
+MatrixXd calculate_tether_vecs(VectorXd COM, MatrixXd teth_anchor, MatrixXd offset) {
+    // Define tether vectors
+    VectorXd tethvec1(3), tethvec2(3), tethvec3(3);
+    VectorXd teth1_hat(3), teth2_hat(3), teth3_hat(3);
 
-
-array<double, 3> equations(const double p[], double a, double b, double c, double r) {
-    const double pi = M_PI; // Use M_PI from <cmath>, or define it if unavailable
-    double eq1 = pow(p[0], 2) + pow(p[1] - (2 - r), 2) + pow(p[2], 2) - pow(a, 2);
-    double eq2 = pow(p[0] - (2 - r) * cos(210 * pi / 180), 2) +
-                 pow(p[1] - (2 - r) * sin(210 * pi / 180), 2) +
-                 pow(p[2], 2) - pow(b, 2);
-    double eq3 = pow(p[0] - (2 - r) * cos(330 * pi / 180), 2) +
-                 pow(p[1] - (2 - r) * sin(330 * pi / 180), 2) +
-                 pow(p[2], 2) - pow(c, 2);
-
-    return {eq1, eq2, eq3}; // Return as a std::array
-}
-double calculate_apex(const double p[], double a, double b, double c, double r){
-    int initial_guess[2] = {2, 2, 4};//  # Start with a point above the base
-    double apex = fsolve(equations, initial_guess, args=(a, b, c, r)); // left out andrew will need to define how he wants this to work
-    return apex;
-}
-
-
-// Norm of a 3D vector
-float norm(const float vec[3]) {
-    return sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
-}
-
-// Vector subtraction
-void subtract(const float a[3], const float b[3], float result[3]) {
-    result[0] = a[0] - b[0];
-    result[1] = a[1] - b[1];
-    result[2] = a[2] - b[2];
-}
-
-// Inverse of a 3x3 matrix
-bool inverse(const float mat[3][3], float result[3][3]) {
-    float det = mat[0][0] * (mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1]) -
-                mat[0][1] * (mat[1][0] * mat[2][2] - mat[1][2] * mat[2][0]) +
-                mat[0][2] * (mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0]);
-
-    if (fabs(det) < 1e-6) {
-        return false; // Matrix is singular
-    }
-
-    float invDet = 1.0 / det;
-
-    result[0][0] = invDet * (mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1]);
-    result[0][1] = invDet * (mat[0][2] * mat[2][1] - mat[0][1] * mat[2][2]);
-    result[0][2] = invDet * (mat[0][1] * mat[1][2] - mat[0][2] * mat[1][1]);
-
-    result[1][0] = invDet * (mat[1][2] * mat[2][0] - mat[1][0] * mat[2][2]);
-    result[1][1] = invDet * (mat[0][0] * mat[2][2] - mat[0][2] * mat[2][0]);
-    result[1][2] = invDet * (mat[0][2] * mat[1][0] - mat[0][0] * mat[1][2]);
-
-    result[2][0] = invDet * (mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0]);
-    result[2][1] = invDet * (mat[0][1] * mat[2][0] - mat[0][0] * mat[2][1]);
-    result[2][2] = invDet * (mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]);
-
-    return true;
-}
-
-// Calculate tether forces
-bool calculate_tether_forces(const float apex[3], float mass, float r, float forces[3]) {
-    const float pi = 3.14159;
-
-    // Tether vectors
-    float teth1_vec[3], teth2_vec[3], teth3_vec[3];
-
- //   subtract(apex, (float[]){0, 2 - r, 0}, teth1_vec);
-teth1_vec[0]=apex[0];
-teth1_vec[1]=apex[1]-(2-r);
-teth1_vec[2]=apex[2];
-
- //   subtract(apex, (float[]){(2 - r) * cos(210 * pi / 180), (2 - r) * sin(210 * pi / 180), 0}, teth2_vec);
-    teth2_vec[0]=apex[0]- (2 - r) * cos(210 * pi / 180); 
-teth2_vec[1]=apex[1]-(2-r)-(2 - r) * sin(210 * pi / 180);
-teth2_vec[2]=apex[2];
-   // subtract(apex, (float[]){(2 - r) * cos(330 * pi / 180), (2 - r) * sin(330 * pi / 180), 0}, teth3_vec);
-  teth3_vec[0]=apex[0]- (2 - r) * cos(330 * pi / 180); 
-teth3_vec[1]=apex[1]-(2-r)-(2 - r) * sin(330 * pi / 180);
-teth3_vec[2]=apex[2];
-    // Normalized vectors
-    float teth1_hat[3] = {teth1_vec[0] / norm(teth1_vec), teth1_vec[1] / norm(teth1_vec), teth1_vec[2] / norm(teth1_vec)};
-    float teth2_hat[3] = {teth2_vec[0] / norm(teth2_vec), teth2_vec[1] / norm(teth2_vec), teth2_vec[2] / norm(teth2_vec)};
-    float teth3_hat[3] = {teth3_vec[0] / norm(teth3_vec), teth3_vec[1] / norm(teth3_vec), teth3_vec[2] / norm(teth3_vec)};
-
-    // Matrix of normalized vectors
-    float M1[3][3] = {
-        {teth1_hat[0], teth2_hat[0], teth3_hat[0]},
-        {teth1_hat[1], teth2_hat[1], teth3_hat[1]},
-        {teth1_hat[2], teth2_hat[2], teth3_hat[2]}
-    };
-
-    // Force vector
-    float M2[3] = {0, 0, mass};
-
-    // Invert the matrix
-    float M1_inv[3][3];
-    if (!inverse(M1, M1_inv)) {
-        return false; // Matrix inversion failed
-    }
-
-    // Multiply M1_inv by M2 to calculate forces
+    // Compute tether vectors
     for (int i = 0; i < 3; i++) {
-        forces[i] = 0;
-        for (int j = 0; j < 3; j++) {
-            forces[i] += M1_inv[i][j] * M2[j];
-        }
+        tethvec1(i) = teth_anchor(0, i) - (COM(i) - offset(0, i));
+        tethvec2(i) = teth_anchor(1, i) - (COM(i) - offset(1, i));
+        tethvec3(i) = teth_anchor(2, i) - (COM(i) - offset(2, i));
     }
 
+    // Normalize tether vectors to get unit vectors
+    teth1_hat = tethvec1 / tethvec1.norm();
+    teth2_hat = tethvec2 / tethvec2.norm();
+    teth3_hat = tethvec3 / tethvec3.norm();
+
+    // Define result matrix (3x4) → Columns: unit vectors, Last row: lengths
+    MatrixXd ANS(3, 4);
+    ANS.col(0) = teth1_hat;
+    ANS.col(1) = teth2_hat;
+    ANS.col(2) = teth3_hat;
+    ANS(0, 3) = tethvec1.norm();  // Lengths
+    ANS(1, 3) = tethvec2.norm();
+    ANS(2, 3) = tethvec3.norm();
+
+    return ANS;
+}
+
+/*
+def calculate_tether_forces(apex, mass, teth_anchor, offset):
+    # solve the system of eqns of the unit vectors to find the equations
+    teth1_hat, teth2_hat, teth3_hat, _ = calculate_tether_vecs(apex, teth_anchor, offset)
+
+    M1 = np.array([[teth1_hat[0], teth2_hat[0], teth3_hat[0]],
+                   [teth1_hat[1], teth2_hat[1], teth3_hat[1]],
+                   [teth1_hat[2], teth2_hat[2], teth3_hat[2]]])
+    M2 = np.array([[0],[0],[mass]])
+    f = np.dot(np.linalg.inv(M1), M2)
+    return f
+
+*/
+
+VectorXd calculate_tether_forces(VectorXd apex,int mass,MatrixXd teth_anchor, MatrixXd offset){
+
+ MatrixXd Initial_Matrix(3, 4) = calculate_tether_vecs(apex, teth_anchor, offset);
+MatrixXd M1(3,3);
+
+for(int i=0;i<3;i++){
+for(int j=0;j<3;j++){
+M1(i,j)= Initial_Matrix(i,j);
+
+}
+
+}
+VectorXd M2(3);
+ M2<<0,0,mass;
+ // Solve for forces using matrix inverse
+    VectorXd F = M1.inverse() * M2;
+
+    return F;  // Return the calculated tether forces
+
+}
+void setup() {
+    // Put your setup code here, it will only run once:
+
+    // Sets all motor connectors to the correct mode for Follow Digital
+    // Torque mode.
+    MotorMgr.MotorModeSet(MotorManager::MOTOR_ALL,
+                          Connector::CPM_MODE_A_DIRECT_B_PWM);
+
+    // Sets up serial communication and waits up to 5 seconds for a port to open
+    // Serial communication is not required for this example to run
+    Serial.begin(baudRate);
+    uint32_t timeout = 5000;
+    uint32_t startTime = millis();
+    while (!Serial && millis() - startTime < timeout) {
+        continue;
+    }
+
+    // Enables the motor
+    motor.EnableRequest(true);
+    Serial.println("Motor Enabled");
+
+    // Waits for HLFB to assert (waits for homing to complete if applicable)
+    Serial.println("Waiting for HLFB...");
+    while (motor.HlfbState() != MotorDriver::HLFB_ASSERTED) {
+        continue;
+    }
+    Serial.println("Motor Ready");
+}
+
+
+void loop() {
+    // Put your main code here, it will run repeatedly:
+
+    // Output 15% of the motor's peak torque in the positive (CCW) direction.
+    CommandTorque(15);    // See below for the detailed function definition.
+    // Wait 2000ms.
+    delay(2000);
+
+    CommandTorque(-75); // Output 75% peak torque in the negative (CW) direction.
+    delay(2000);
+
+    CommandTorque(5); // Output 5% peak torque in the positive (CCW) direction.
+    delay(2000);
+
+    CommandTorque(-35); // Output 35% peak torque in the negative (CW) direction.
+    delay(2000);
+
+    CommandTorque(10); // Output 10% peak torque in the positive (CCW) direction.
+    delay(2000);
+}
+
+/*------------------------------------------------------------------------------
+ * CommandTorque
+ *
+ *    Command the motor to move using a torque of commandedTorque
+ *    Prints the move status to the USB serial port
+ *    Returns when HLFB asserts (indicating the motor has reached the commanded
+ *    torque)
+ *
+ * Parameters:
+ *    int commandedTorque  - The torque to command
+ *
+ * Returns: True/False depending on whether the torque was successfully
+ * commanded.
+ */
+bool CommandTorque(int commandedTorque) {
+    if (abs(commandedTorque) > abs(maxTorque)) {
+        Serial.println("Move rejected, invalid torque requested");
+        return false;
+    }
+	
+    Serial.print("Commanding torque: ");
+    Serial.println(commandedTorque);
+
+    // Find the scaling factor of our torque range mapped to the PWM duty cycle
+    // range (255 is the max duty cycle).
+    double scaleFactor = 255 / maxTorque;
+
+    // Scale the torque command to our duty cycle range.
+    int dutyRequest = abs(commandedTorque) * scaleFactor;
+
+    // Set input A to match the direction of torque.
+    if (commandedTorque < 0) {
+        motor.MotorInAState(true);
+    }
+    else {
+        motor.MotorInAState(false);
+    }
+
+    // Ensures this delay is at least 20ms longer than the Input A filter
+    // setting in MSP
+    delay(20 + INPUT_A_FILTER);
+
+    // Command the move
+    motor.MotorInBDuty(dutyRequest);
+
+    // Waits for HLFB to assert (signaling the move has successfully completed)
+    Serial.println("Moving... Waiting for HLFB");
+    // Allow some time for HLFB to transition
+    delay(1);
+    while (motor.HlfbState() != MotorDriver::HLFB_ASSERTED) {
+        continue;
+    }
+
+    Serial.println("Move Done");
     return true;
-}
-
-// Main loop
-
-
-float dot_product(const float a[3], const float b[3]) {
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-// Calculate tether error
-void calculate_tether_error(const float COM[3], const float f[3], float mass, float r,
-                            float &f_err, float &angle_err, float teth1_vec[3], float teth2_vec[3], float teth3_vec[3]) {
-    const float pi = 3.14159;
-
-    // Compute tether attachment locations
-    float teth1_attach_loc[3] = {COM[0], COM[1] + r, COM[2]};
-    float teth2_attach_loc[3] = {COM[0] + r * cos(210 * pi / 180), COM[1] + r * sin(210 * pi / 180), COM[2]};
-    float teth3_attach_loc[3] = {COM[0] + r * cos(330 * pi / 180), COM[1] + r * sin(330 * pi / 180), COM[2]};
-
-    // Compute tether initial vectors
-    float teth1_init_vec[3] = {0 - teth1_attach_loc[0], 2 - teth1_attach_loc[1], 0 - teth1_attach_loc[2]};
-    float teth2_init_vec[3] = {2 * cos(210 * pi / 180) - teth2_attach_loc[0], 
-                               2 * sin(210 * pi / 180) - teth2_attach_loc[1], 
-                               0 - teth2_attach_loc[2]};
-    float teth3_init_vec[3] = {2 * cos(330 * pi / 180) - teth3_attach_loc[0], 
-                               2 * sin(330 * pi / 180) - teth3_attach_loc[1], 
-                               0 - teth3_attach_loc[2]};
-
-    // Normalize tether initial vectors
-    float teth1_norm = norm(teth1_init_vec);
-    float teth2_norm = norm(teth2_init_vec);
-    float teth3_norm = norm(teth3_init_vec);
-
-    // Compute tether force vectors
-    for (int i = 0; i < 3; i++) {
-        teth1_vec[i] = f[0] * (teth1_init_vec[i] / teth1_norm);
-        teth2_vec[i] = f[1] * (teth2_init_vec[i] / teth2_norm);
-        teth3_vec[i] = f[2] * (teth3_init_vec[i] / teth3_norm);
-    }
-
-    // Compute resulting force vector
-    float f_vec[3] = {
-        teth1_vec[0] + teth2_vec[0] + teth3_vec[0],
-        teth1_vec[1] + teth2_vec[1] + teth3_vec[1],
-        teth1_vec[2] + teth2_vec[2] + teth3_vec[2]
-    };
-
-    // Expected force vector
-    float expected_f_vec[3] = {0, 0, -mass};
-
-    // Compute errors
-    float f_vec_norm = norm(f_vec);
-    float expected_f_vec_norm = norm(expected_f_vec);
-
-    if (f_vec_norm > 0 && expected_f_vec_norm > 0) {
-        angle_err = acos(dot_product(expected_f_vec, f_vec) / (expected_f_vec_norm * f_vec_norm)) * (180 / pi);
-    } else {
-        angle_err = 0;
-    }
-
-    f_err = fabs(expected_f_vec_norm - f_vec_norm);
 }
