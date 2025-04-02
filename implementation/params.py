9536606c125f589a389a0e89f3deb940b5330598
pejo3543 <pejo3543@colorado.edu>
2025-04-02 15:01:40 -0600

    updated physical parameters

--- implementation/params.py
+++ implementation/params.py
@@ -5,7 +5,7 @@ class two_Teth_Parameters:
     # r = 2 / (2 * np.pi)  # waist radius (ft)
     # waist_height = -2.5
     mass = 200.0  # person's weight (lb)
-    r = 3 / (2 * np.pi)  # waist radius (ft)
+    r = 6.0/12.0  # waist radius (ft)
     waist_height = -3.1
 
     # tether anchor loc 3x3 each row is the vector for each tether
@@ -19,16 +19,21 @@ class two_Teth_Parameters:
 
 class three_teth_Parameters:
     mass = 100.0  # person's weight (lb)
-    r = 2 / (2 * np.pi)  # waist radius (ft)
+    r = 6.0/12.0  # waist radius (ft)
     waist_height = -2.5
     # mass = 200.0  # person's weight (lb)
     # r = 3 / (2 * np.pi)  # waist radius (ft)
     # waist_height = -3.1
 
     # tether anchor loc 3x3 each row is the vector for each tether
-    teth_anchor = [[-2,                      0.0,                     0.0],
-                  [2.0, -2.0, 0.0],
-                  [2.0, 2.0, 0.0]]
+    # teth_anchor = [[-2,                      0.0,                     0.0],
+    #               [2.0, -2.0, 0.0],
+    #               [2.0, 2.0, 0.0]]
+
+    teth_anchor = [[-22.0/12.0, 0.5625/12.0, -11.125/12.0],
+                   [22.0/12.0, -27.0625/12.0, -11.125/12.0],
+                   [22.0/12.0, 27.0625/12.0, -11.125/12.0]]
+
     offset = [[r,                      0.0,                     0.0],
               [-r*np.cos(315*np.pi/180), -r*np.sin(315*np.pi/180), 0.0],
               [-r*np.cos(45*np.pi/180), -r*np.sin(45*np.pi/180), 0.0]]
--- modeling/dynamic_models/Parameters_3_Tether_Dynamic_Model.py
+++ modeling/dynamic_models/Parameters_3_Tether_Dynamic_Model.py
@@ -2,13 +2,17 @@ import numpy as np
 
 class Parameters:
     mass = 200.0  # person's weight (lb)
-    r = 3 / (2 * np.pi)  # waist radius (ft)
+    r = 6/12.0  # waist radius (ft)
     # waist_height = -2.5
     # mass = 200.0  # person's weight (lb)
     # r = 3 / (2 * np.pi)  # waist radius (ft)
     waist_height = -3.0
 
     # tether anchor loc 3x3 each row is the vector for each tether
+    # teth_anchor = [[-2,                      0.0,                     -0.1],
+    #               [2.0, -2.0, -0.1],
+    #               [2.0, 2.0, -0.1]]
+
     teth_anchor = [[-22.0/12.0, 0.5625/12.0, -11.125/12.0],
                    [22.0/12.0, -27.0625/12.0, -11.125/12.0],
                    [22.0/12.0, 27.0625/12.0, -11.125/12.0]]
